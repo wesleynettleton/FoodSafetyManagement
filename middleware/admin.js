95891d8087ac24766138ee0afee0c2d2cd37fec1
Wesley Nettleton <wesleynettleton@Wesleys-MacBook-Pro.local>
2025-05-20 13:12:25 +0100

    Revert "Add admin middleware for role-based access control"
    This reverts commit 0a6da41e6174c3872bca680555737e066c3b0c57.

--- middleware/admin.js
+++
@@ -1,9 +0,0 @@
-const admin = (req, res, next) => {
-  if (req.user && req.user.role === 'admin') {
-    next();
-  } else {
-    res.status(403).json({ message: 'Access denied. Admin privileges required.' });
-  }
-};
-
-module.exports = admin; 
